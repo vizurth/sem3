428049b156db09a18da9a04c1cf5afa7ae279e32
vizurth <timrsamara@gmail.com>
2025-09-05 09:25:09 +0300

    feat: arch and makefile

--- oop/lab2/src/headers/stdafx.h
+++ oop/lab2/src/headers/stdafx.h
@@ -3,8 +3,7 @@
 #include <iostream>	 // Эта - часть библиотеки STL (Standard Template Library)
 #include <limits>	 // Полезные константы
 #include <cstring>
-#include <intrin.h> // Для __nop
 
 using namespace std;	 // Обеспечивает видимость имен из STL
 
-#define	  stop __nop();
+#define	stop
