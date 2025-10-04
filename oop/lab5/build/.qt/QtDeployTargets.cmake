5965eb9e6aa4a3d525e68b774313c59d8c4dcebb
vizurth <timrsamara@gmail.com>
2025-10-04 14:05:01 +0300

    feat: done lab5

---
+++ oop/lab5/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_HelloWorld_FILE /Users/timuradiatullin/narnia/spbpu/sem3/oop/lab5/build/HelloWorld)
+set(__QT_DEPLOY_TARGET_HelloWorld_TYPE EXECUTABLE)
---
+++ oop/lab5/src/headers/mainwindow.h
@@ -0,0 +1,25 @@
+#include <Qwidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QApplication>
+
+
+class MainWindow : public QWidget {
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget* parent = nullptr) : QWidget(parent) {
+		setWindowTitle("Hello World App");
+
+		setFixedSize(400, 200);
+
+		QLabel* label = new QLabel("Hello World!", this);
+
+		label->setGeometry(150, 50, 100, 30);
+
+    QPushButton* button = new QPushButton("Exit", this);
+    button->setGeometry(150, 100, 100, 30);
+
+		QObject::connect(button, &QPushButton::clicked, QApplication::instance(), &QApplication::quit);
+	}
+ };
