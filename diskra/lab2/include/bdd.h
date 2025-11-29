39961091e03a5ab13e6a4f0ad58e89e35b2f00e1
vizurth <81080666+vizurth@users.noreply.github.com>
2025-11-29 18:32:41 +0300

    refactor: lab2

--- diskra/lab2/include/bdd.h
+++ diskra/lab2/include/bdd.h
@@ -41,31 +41,22 @@ public:
     
     // Построение BDD из вашей диаграммы
     void buildFromDiagram() {
-        // Терминальные узлы (листья)
-        addNode(0, "0", -1, -1, true);   // Терминал 0 (id, variable, low, high, isTerminal)
-        addNode(1, "1", -1, -1, true);   // Терминал 1
+        // листья
+        addNode(0, "0", -1, -1, true);   // 0
+        addNode(1, "1", -1, -1, true);   // 1
         
-        // Узлы уровня x3 (два разных узла с переменной x3)
-        // x3_left: low->0, high->1
         addNode(2, "x3", 1, 0, false);
         
-        // x3_right: low->1, high->1
         addNode(3, "x3", 0, 1, false);
         
-        // Узлы уровня x4 (два разных узла с переменной x4)
-        // x4_left: low->x3_left(2), high->x3_right(3)
         addNode(4, "x4", 2, 3, false);
         
-        // x4_right: low->x3_right(3), high->x3_left(2)
         addNode(5, "x4", 3, 1, false);
         
-        // Узел x2: low->x4_left(4), high->x4_right(5)
         addNode(6, "x2", 4, 5, false);
         
-        // Корневой узел x1: low->x3_left(2), high->x2(6)
         addNode(7, "x1", 2, 6, false);
         
-        // Устанавливаем корень
         setRoot(7);
     }
     
