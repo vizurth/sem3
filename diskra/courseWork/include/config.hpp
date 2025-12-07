75ad282fcabdfaf6190ea0ca611c126cb7031111
vizurth <81080666+vizurth@users.noreply.github.com>
2025-12-07 13:39:06 +0300

    refactor: courseWork

--- diskra/courseWork/include/config.hpp
+++ diskra/courseWork/include/config.hpp
@@ -8,13 +8,13 @@ using namespace std;
 
 const int N = 8;
 const map<string, string> plusOne = {
-		{"a", "b"},
-		{"b", "c"},
-		{"c", "e"},
-		{"d", "g"},
-		{"e", "d"},
-		{"f", "h"},
-		{"g", "f"},
-		{"h", "a"},
+		{"0", "1"},
+		{"1", "2"},
+		{"2", "3"},
+		{"3", "4"},
+		{"4", "5"},
+		{"5", "6"},
+		{"6", "7"},
+		{"7", "0"},
 };
-const vector<string> alph = {"a", "b", "c", "d", "e", "f", "g", "h"};
+const vector<string> alph = {"0", "1", "2", "3", "4", "5", "6", "7"};
